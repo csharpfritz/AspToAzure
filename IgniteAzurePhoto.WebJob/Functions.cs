68915c0b0fca0eb87ad274be8fa195fc9c1a4eb0
Jeffrey T. Fritz <jeffreytfritz@gmail.com>
2015-05-05 15:23:29 -0500

    Final update from stage

---
+++ IgniteAzurePhoto.WebJob/Functions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Helpers;
+using ImageResizer.Configuration;
+using Microsoft.Azure.WebJobs;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace IgniteAzurePhoto.WebJob
+{
+  public class Functions
+  {
+    private static Config _Config;
+
+    static Functions()
+    {
+      // Warm up the image resizer object
+      var ar = new ImageResizer.Plugins.Basic.AutoRotate();
+      _Config = new ImageResizer.Configuration.Config();
+      ar.Install(_Config);
+      
+    }
+
+    public static void ResizeThumbnail(
+      [BlobTrigger(@"upload/{name}")] Stream inputStream,
+      [Blob(@"thumbnail/{name}", FileAccess.Write)] Stream thumbnailBlob)
+    {
+
+      // Thumbnail
+      var outImage = _Config.CurrentImageBuilder.Build(inputStream, new ImageResizer.ResizeSettings(@"maxwidth=200&maxheight=200&format=jpg&autorotate=true"));
+      outImage.Save(thumbnailBlob, ImageFormat.Jpeg);
+    }
+
+    public static void ResizeMidsize(
+      [BlobTrigger(@"upload/{name}")] Stream inputStream,
+      [Blob(@"full/{name}", FileAccess.Write)] Stream midsizeBlob)
+    {
+
+      // midsize
+      var midImage = _Config.CurrentImageBuilder.Build(inputStream, new ImageResizer.ResizeSettings(@"maxwidth=1200&maxheight=1200&format=jpg&autorotate=true"));
+      midImage.Save(midsizeBlob, ImageFormat.Jpeg);
+
+    }
+
+  }
+}
---
+++ IgniteAzurePhoto.WebJob/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Helpers;
+using ImageResizer.Configuration;
+using Microsoft.Azure.WebJobs;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace IgniteAzurePhoto.WebJob
+{
+  // To learn more about Microsoft Azure WebJobs SDK, please see http://go.microsoft.com/fwlink/?LinkID=320976
+  class Program
+  {
+
+    // Please set the following connection strings in app.config for this WebJob to run:
+    // AzureWebJobsDashboard and AzureWebJobsStorage
+    static void Main()
+    {
+      var host = new JobHost();
+
+
+      // The following code ensures that the WebJob will be running continuously
+      host.RunAndBlock();
+
+      //host.Call(typeof(Functions).GetMethod("ResizeThumbnail"));
+      //host.Call(typeof(Functions).GetMethod("ResizeMidsize"));
+
+    }
+
+
+  }
+}
